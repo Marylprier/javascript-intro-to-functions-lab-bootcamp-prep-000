752a43d90e4629d0ba63c493c7d5307ca916910d
Mary Prier <priertroyandmary@gmail.com>
2018-02-05 18:49:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase())
-  return "I can't hear you!"
-}  elseif (string === string.toUpperCase())
-  console.log("YES INDEED!")
-  else (string === "I love you, Grandma.")
-  return "I love you, too."
+    return "I can't hear you!"
+} elseif (string === string.toUpperCase())
+    console.log("YES INDEED!")
+} else {
+    (string === "I love you, Grandma.")
+    return "I love you, too."
 }
