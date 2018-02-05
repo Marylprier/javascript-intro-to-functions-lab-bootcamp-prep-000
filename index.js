1b79114f9815f534b934ea4da255a4974d8416d9
Mary Prier <priertroyandmary@gmail.com>
2018-02-05 18:46:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function sayHiToGrandma(string) {
   return "I can't hear you!"
 }  elseif (string === string.toUpperCase())
   console.log("YES INDEED!")
-}  else (string === "I love you, Grandma.")
+  else (string === "I love you, Grandma.")
   return "I love you, too."
 }
